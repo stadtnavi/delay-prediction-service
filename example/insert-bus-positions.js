306da328713026e4b082f1a6d58294d9708c131e
Jannis R <mail@jannisr.de>
2021-05-06 21:11:08 +0200

    receive geofence events from Tile38

--- example/insert-bus-positions.js
+++ example/insert-bus-positions.js
@@ -5,6 +5,10 @@ const {parse} = require('ndjson')
 const buffer = require('@turf/buffer')
 const truncate = require('@turf/truncate').default
 
+const TTL = process.env.TTL
+	? parseInt(process.env.TTL)
+	: 60 * 60 // 1h
+
 const showError = (err) => {
 	console.error(err)
 	process.exit(1)
@@ -24,6 +28,13 @@ process.stdin
 
 	process.stdout.write([
 		'SET', 'buses', vehicleId,
+		// Pass through original lon/lat by adding them as fields.
+		// https://tile38.com/commands/set#fields
+		// Apparently the fields `lon` & `lat` are reserved, so we picked different ones.
+		'FIELD', 'lo', longitude,
+		'FIELD', 'la', latitude,
+		// todo: add speed as field?
+		// todo: add pax as field?
 		'OBJECT', JSON.stringify(shape),
 	].join(' ') + '\n')
 })
--- index.js
+++ index.js
@@ -1,3 +1,49 @@
 'use strict'
 
-// todo
+const Redis = require('ioredis')
+const logger = require('./lib/logger')
+
+const showError = (err) => {
+	logger.error(err)
+	process.exit(1)
+}
+
+// Using subscriptions has no backpressure mechanism, so this process may
+// read more events into memory than it can process, crashing eventually.
+// > The backpressure pattern consists of a feedback mechanism that allows
+// > consumers to inform upstream components when they are ready to handle
+// > new messages, preventing them from becoming overwhelmed or stressed.
+// https://developer.ibm.com/depmodels/reactive-systems/articles/kafka-fit-reactive-system/
+// todo: let Tile38 write into a message broker, consume events from there
+
+const startReceivingGeofenceEvents = async (onEventMsg) => {
+	const tile38 = new Redis(process.env.TILE38_URL || 'redis://localhost:9851/')
+
+	const stop = async () => {
+		tile38.removeListener('pmessage', onEventMsg)
+		await tile38.punsubscribe('*')
+	}
+
+	tile38.on('pmessage', onEventMsg)
+	const nrOfSubscribedChannels = await tile38.psubscribe('*')
+	logger.debug(`subscribed to ${nrOfSubscribedChannels} channels`)
+
+	return stop
+}
+
+startReceivingGeofenceEvents((_, channel, event) => {
+	const shapeId = channel
+	event = JSON.parse(event)
+	if (!event.fields) {
+		logger.error({event}, 'event has no fields')
+		return; // todo: what to do here?
+	}
+	const {
+		detect,
+		id: vehicleId,
+		fields: {lo: longitude, la: latitude},
+	} = event
+
+	// todo
+})
+.catch(showError)
