4d8911049ed78a303d2567391f9fbdae124f46c9
Jannis R <mail@jannisr.de>
2021-05-14 20:57:13 +0200

    receive Thingsboard vehicle positions via WebSocket

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 'use strict'
 
+const subscribeToVehiclePositions = require('./lib/vehicle-positions')
+
 // todo
+subscribeToVehiclePositions()
+.on('data', console.log)
+.on('error', console.error)
