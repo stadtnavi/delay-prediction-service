b806f3988d5e8d9ec0215bc719e2d20938e42392
Jannis R <mail@jannisr.de>
2021-05-14 22:22:13 +0200

    on vehicle position, match shapes

--- deploy.sql
+++ deploy.sql
@@ -32,33 +32,47 @@ CREATE TABLE vehicle_positions (
 CREATE INDEX ON vehicle_positions (vehicle_id, t);
 -- CREATE INDEX ON vehicle_positions USING GIST (location);
 
-CREATE TYPE shape_matching AS (
+CREATE TYPE matched_vehicle AS (
+	trip_id TEXT,
+	date TIMESTAMP,
 	shape_id TEXT,
 	nr_of_consec_vehicle_pos BIGINT
 );
 
-CREATE FUNCTION all_shapes_matching_vehicle_positions(
+CREATE FUNCTION all_vehicle_matches(
 	_yesterday date,
 	_today date,
-	_t_min timestamptz,
-	_t_max timestamptz,
+	_t_arrival_min timestamptz,
+	_t_arrival_max timestamptz,
+	_t_vehicle_pos_min timestamptz,
+	_t_vehicle_pos_max timestamptz,
 	_vehicle_id text
 )
-RETURNS SETOF shape_matching
+RETURNS SETOF matched_vehicle
 AS $$
+	-- todo: this is ugly, clean it up
+	SELECT *
+	FROM (
 	SELECT
+		DISTINCT ON (shapes.shape_id, vehicle_id)
+		shape_ids.trip_id,
+		shape_ids.date,
 		shapes.shape_id,
-		count(pos.pos_id) as nr_of_consec_vehicle_pos
+		count(pos.pos_id) OVER (PARTITION BY shapes.shape_id ORDER BY trip_id) as nr_of_consec_vehicle_pos
 	FROM (
 		SELECT DISTINCT ON (trips.shape_id)
-			trips.shape_id
+			trips.shape_id,
+			trips.trip_id,
+			date
 		FROM arrivals_departures
 		INNER JOIN trips ON trips.trip_id = arrivals_departures.trip_id
 		WHERE True
 		-- cut off by date for better performance
-		-- todo: use range from t_min to t_max instead of hardcoded values
 		AND (date = _yesterday OR date = _today)
-		AND t_arrival >= _t_min AND t_arrival <= _t_max
+		-- We're just trying to find any currently active "runs" here, so it doesn't
+		-- matter if we're excluding some of their arrivals/departures. We just want
+		-- to find *any* arrival/departure of the "run".
+		AND t_arrival >= _t_arrival_min AND t_arrival <= _t_arrival_max
 	) shape_ids
 	INNER JOIN shapes_aggregated shapes ON shapes.shape_id = shape_ids.shape_id
 	INNER JOIN (
@@ -71,36 +85,44 @@ AS $$
 		FROM vehicle_positions
 		WHERE True
 		AND vehicle_id = _vehicle_id
-		AND t >= _t_min AND t <= _t_max
+		-- When filtering vehicle positions, we pick a longer time range. If the
+		-- vehicle has a delay of n minutes, we still need to find its positions
+		-- *older* than n minutes, in order to reliably identify its "run".
+		AND t >= _t_vehicle_pos_min AND t <= _t_vehicle_pos_max
+		ORDER BY t DESC
 	) pos ON st_dwithin(shapes.shape, pos.location, pos.hdop)
-	GROUP BY shapes.shape_id, vehicle_id
+	) t
 	ORDER BY nr_of_consec_vehicle_pos DESC
 	LIMIT 2;
 $$ LANGUAGE sql;
 
-CREATE FUNCTION shape_matching_vehicle_positions(
+CREATE FUNCTION vehicle_match(
 	_yesterday date,
 	_today date,
-	_t_min timestamptz,
-	_t_max timestamptz,
+	_t_arrival_min timestamptz,
+	_t_arrival_max timestamptz,
+	_t_vehicle_pos_min timestamptz,
+	_t_vehicle_pos_max timestamptz,
 	_vehicle_id text
 )
-RETURNS shape_matching
+RETURNS matched_vehicle
 AS $$
 	DECLARE
-		_sm shape_matching;
-		_first shape_matching;
+		_sm matched_vehicle;
+		_first matched_vehicle;
 	BEGIN
 		FOR _sm IN
-			SELECT * FROM all_shapes_matching_vehicle_positions(
+			SELECT * FROM all_vehicle_matches(
 				_yesterday,
 				_today,
-				_t_min,
-				_t_max,
+				_t_arrival_min,
+				_t_arrival_max,
+				_t_vehicle_pos_min,
+				_t_vehicle_pos_max,
 				_vehicle_id
 			)
 		LOOP
-			-- all_shapes_matching_vehicle_positions returns <= 2 results
+			-- all_vehicle_matches returns <= 2 results
 			IF _first IS NULL THEN
 				_first := _sm;
 			ELSEIF _sm.nr_of_consec_vehicle_pos = _first.nr_of_consec_vehicle_pos THEN
--- index.js
+++ index.js
@@ -37,7 +37,46 @@ const processVehiclePosition = async (db, vehiclePos) => {
 		dt.toISO(),
 	])
 
-	// todo
+	const now = DateTime.fromMillis(Date.now(), {zone: TIMEZONE, locale: LOCALE})
+	const {rows: [match]} = await db.query(`
+		SELECT
+			trip_id,
+			date,
+			shape_id,
+			nr_of_consec_vehicle_pos
+		FROM vehicle_match($1, $2, $3, $4, $5, $6, $7)
+	`, [
+		// yesterday & today
+		now.toISODate(), now.minus({days: 1}).toISODate(),
+		// t_arrival_min
+		// The vehicle may end a "run" up to 60m behind schedule.
+		now.minus({minutes: 60}).toISO(),
+		// t_arrival_max
+		// The vehicle may start a "run" up to 20m ahead of schedule.
+		now.plus({minutes: 20}).toISO(),
+		// t_vehicle_pos_min
+		// The vehicle may be up to 60m behind schedule.
+		now.minus({minutes: 60}).toISO(),
+		// t_vehicle_pos_max
+		// The vehicle may be up to 20m ahead of schedule.
+		now.plus({minutes: 60}).toISO(),
+		// vehicle_id
+		vehicleId,
+	])
+	const {trip_id, date, shape_id, nr_of_consec_vehicle_pos} = match
+	if (!trip_id) {
+		logger.info({vehicleId}, 'vehicle has no match')
+		return;
+	}
+	logger.info({
+		vehicleId,
+		trip_id,
+		date,
+		shape_id,
+		nrOfMatchingVehiclePositions: parseInt(nr_of_consec_vehicle_pos),
+	}, 'vehicle has a match!')
+
+	// todo: do something with it here
 }
 
 subscribeToVehiclePositions()
