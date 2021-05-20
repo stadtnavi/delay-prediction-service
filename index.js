75f44f09860215520c79c67e0a32f1ce7e9ded53
Jannis R <mail@jannisr.de>
2021-05-20 18:30:09 +0100

    matching WIP

--- index.js
+++ index.js
@@ -1,24 +1,54 @@
 'use strict'
 
 const {Transform} = require('stream')
+const {DateTime} = require('luxon')
 const logger = require('./lib/logger')
 const prognoseRunDelays = require('./lib/prognose-delays')
 const subscribeToVehiclePositions = require('./lib/vehicle-positions')
 const {runWithinTx} = require('./lib/db')
+
+const TIMEZONE = process.env.TIMEZONE
+if (!TIMEZONE) {
+	console.error('Missing/empty TIMEZONE environment variable.')
+	process.exit(1)
+}
+const LOCALE = process.env.LOCALE
+if (!LOCALE) {
+	console.error('Missing/empty LOCALE environment variable.')
+	process.exit(1)
+}
 
 const abortWithError = (err) => {
 	logger.error(err)
 	process.exit(1)
 }
 
-const processVehiclePosition = async (vehiclePos) => {
-	logger.debug({vehiclePos})
+const processVehiclePosition = async (db, vehiclePos) => {
 	if (vehiclePos.hdop < 0) console.error('weird data', vehiclePos)
-	console.error('event timestamp', new Date(vehiclePos.t).toISOString(), 'event delay', (Date.now() - vehiclePos.t) / 1000, 's')
+	const {vehicleId, longitude, latitude, hdop, t} = vehiclePos
 
-	await runWithinTx(async (db) => {
-		await prognoseRunDelays(db, vehiclePos) // todo: use the result
-	})
+	const dt = DateTime.fromMillis(t, {zone: TIMEZONE, locale: LOCALE})
+	logger.info({
+		vehicleId,
+		longitude,
+		latitude,
+		hdop,
+		t: dt.toISO(),
+	}, 'inserting vehicle position')
+	await db.query(`
+		INSERT INTO vehicle_positions (vehicle_id, location, hdop, t)
+		VALUES ($1, $2, $3, $4)
+		ON CONFLICT ON CONSTRAINT vehicle_positions_unique DO UPDATE SET
+			location = EXCLUDED.location,
+			hdop = EXCLUDED.hdop;
+	`, [
+		vehicleId,
+		`POINT(${longitude} ${latitude})`,
+		hdop,
+		dt.toISO(),
+	])
+
+	await prognoseRunDelays(db, vehiclePos) // todo: use the result
 }
 
 subscribeToVehiclePositions()
@@ -26,7 +56,9 @@ subscribeToVehiclePositions()
 .pipe(new Transform({
 	objectMode: true,
 	transform: (vehiclePos, _, cb) => {
-		processVehiclePosition(vehiclePos)
+		runWithinTx(async (db) => {
+			await processVehiclePosition(db, vehiclePos)
+		})
 		.then(() => cb(), cb)
 	}
 }))
