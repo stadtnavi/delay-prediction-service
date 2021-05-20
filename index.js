2af8af80908a4b7edf18cf3a79a39e2397a8c488
Jannis R <mail@jannisr.de>
2021-05-20 18:53:17 +0100

    refactor

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
 'use strict'
 
 const {Transform} = require('stream')
-const {DateTime} = require('luxon')
 const logger = require('./lib/logger')
+const insertVehiclePosition = require('./lib/insert-vehicle-position')
 const prognoseRunDelays = require('./lib/prognose-delays')
 const subscribeToVehiclePositions = require('./lib/vehicle-positions')
 const {runWithinTx} = require('./lib/db')
-
-const TIMEZONE = process.env.TIMEZONE
-if (!TIMEZONE) {
-	console.error('Missing/empty TIMEZONE environment variable.')
-	process.exit(1)
-}
-const LOCALE = process.env.LOCALE
-if (!LOCALE) {
-	console.error('Missing/empty LOCALE environment variable.')
-	process.exit(1)
-}
 
 const abortWithError = (err) => {
 	logger.error(err)
@@ -24,30 +13,13 @@ const abortWithError = (err) => {
 }
 
 const processVehiclePosition = async (db, vehiclePos) => {
-	if (vehiclePos.hdop < 0) console.error('weird data', vehiclePos)
-	const {vehicleId, longitude, latitude, hdop, t} = vehiclePos
+	if (vehiclePos.hdop < 0) {
+		// todo: find the root cause
+		logger.warn(vehiclePos, 'ignoring vehicle position because of weird hdop')
+		return;
+	}
 
-	const dt = DateTime.fromMillis(t, {zone: TIMEZONE, locale: LOCALE})
-	logger.info({
-		vehicleId,
-		longitude,
-		latitude,
-		hdop,
-		t: dt.toISO(),
-	}, 'inserting vehicle position')
-	await db.query(`
-		INSERT INTO vehicle_positions (vehicle_id, location, hdop, t)
-		VALUES ($1, $2, $3, $4)
-		ON CONFLICT ON CONSTRAINT vehicle_positions_unique DO UPDATE SET
-			location = EXCLUDED.location,
-			hdop = EXCLUDED.hdop;
-	`, [
-		vehicleId,
-		`POINT(${longitude} ${latitude})`,
-		hdop,
-		dt.toISO(),
-	])
-
+	await insertVehiclePosition(db, vehiclePos)
 	await prognoseRunDelays(db, vehiclePos) // todo: use the result
 }
 
