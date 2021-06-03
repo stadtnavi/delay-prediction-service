c96440d999953d5714d651d825e56ca2745bcf83
Jannis R <mail@jannisr.de>
2021-06-03 22:29:53 +0100

    refactor

--- index.js
+++ index.js
@@ -3,16 +3,10 @@
 const {Transform, pipeline} = require('stream')
 const logger = require('./lib/logger')
 const insertVehiclePosition = require('./lib/insert-vehicle-position')
-const matchVehicle = require('./lib/match-vehicle-positions')
-const fetchRun = require('./lib/fetch-run')
-const prognoseRunDelay = require('./lib/prognose-run-delay')
+const prognoseVehiclePosition = require('./lib/prognose-vehicle-position')
+const prognoseTripUpdate = require('./lib/prognose-trip-update')
 const subscribeToVehiclePositions = require('./lib/vehicle-positions-source')
 const {runWithinTx} = require('./lib/db')
-const estimateVehiclePos = require('./lib/estimate-vehicle-position')
-const {
-	publishTripUpdate,
-	publishVehiclePosition,
-} = require('./lib/publish-realtime-data')
 
 // https://developers.google.com/transit/gtfs-realtime/reference/#enum-schedulerelationship
 // enum ScheduleRelationship
@@ -28,95 +22,14 @@ const processVehiclePosition = async (db, vehiclePosEv) => {
 		logger.warn(vehiclePosEv, 'ignoring vehicle position because of weird hdop')
 		return;
 	}
-	const {vehicleId} = vehiclePosEv
+	const {vehicleId, t: tVehiclePos} = vehiclePosEv
 
 	await insertVehiclePosition(db, vehiclePosEv)
-	const runMatch = await matchVehicle(db, vehicleId, vehiclePosEv.t)
-	if (!runMatch) return; // abort
-	const {
-		latestVehiclePos, tLatestVehiclePos, matchingConsecutiveVehiclePositions,
-		trip_id, date, shape_id,
-	} = runMatch
 
-	const run = await fetchRun(db, trip_id, date)
-	const {
-		route_id,
-		shape,
-		arrivalsDepartures: arrsDeps,
-	} = run
-
-	const {
-		delay,
-		prevOrCurrentArrDep, currentOrNextArrDep,
-	} = await prognoseRunDelay(
-		db,
-		run,
-		vehicleId, latestVehiclePos, tLatestVehiclePos,
-	)
-	if (delay === null) {
-		logger.warn({run, delay}, 'unknown delay, aborting')
-		return; // abort
-	}
-
-	const tripDescriptor = {
-		trip_id,
-		route_id,
-		schedule_relationship: SCHEDULED,
-	}
-	const vehicleDescriptor = {
-		id: vehicleId,
-	}
-
-	// build GTFS-Realtime TripUpdate
-	// https://developers.google.com/transit/gtfs-realtime/reference/#message-tripupdate
-	const tripUpdate = {
-		timestamp: isoToPosix(tLatestVehiclePos),
-		trip: tripDescriptor,
-		vehicle: vehicleDescriptor,
-		// delay,
-		stop_time_update: arrsDeps.map((ad) => {
-			// don't add delays to past arrivals/departures
-			const withDelay = ad.stop_sequence >= prevOrCurrentArrDep.stop_sequence
-			return {
-				stop_sequence: ad.stop_sequence,
-				stop_id: ad.stop_id,
-				arrival: ad.t_arrival ? {
-					time: isoToPosix(ad.t_arrival) + (withDelay ? delay : 0),
-					delay: withDelay ? delay : null,
-					// todo: add uncertainty
-				} : {time: null},
-				departure: ad.t_departure ? {
-					time: isoToPosix(ad.t_departure) + (withDelay ? delay : 0),
-					delay: withDelay ? delay : null,
-					// todo: add uncertainty
-				} : {time: null},
-				schedule_relationship: SCHEDULED,
-			}
-		}),
-	}
-	logger.debug({tripUpdate}, 'built GTFS-Realtime TripUpdate')
-
-	const tNow = Date.now()
-	const estimatedVehiclePos = estimateVehiclePos(arrsDeps, shape, latestVehiclePos, tLatestVehiclePos, tNow)
-
-	// build GTFS-Realtime VehiclePosition
-	// https://developers.google.com/transit/gtfs-realtime/reference/#message-vehicleposition
-	const vehiclePosition = {
-		timestamp: tNow / 1000 | 0,
-		trip: tripDescriptor,
-		vehicle: vehicleDescriptor,
-		position: {
-			latitude: estimatedVehiclePos.coordinates[1],
-			longitude: estimatedVehiclePos.coordinates[0],
-			// todo: bearing & odometer, using run's shape
-		},
-		// todo: current_stop_sequence, stop_id, current_status
-		// todo: congestion_level?
-		// todo: occupancy_status using pax count?
-	}
-
-	await publishTripUpdate(tripUpdate)
-	await publishVehiclePosition(vehiclePosition)
+	await Promise.all([
+		prognoseTripUpdate(db, vehicleId, tVehiclePos),
+		prognoseVehiclePosition(db, vehicleId, tVehiclePos),
+	])
 }
 
 pipeline(
