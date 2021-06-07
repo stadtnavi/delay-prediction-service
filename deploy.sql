6bf613cc3d8a271632f3c9728e8970a3ae649464
Jannis R <mail@jannisr.de>
2021-06-07 22:27:03 +0200

    match by space & time, simultaneously & within JS

--- deploy.sql
+++ deploy.sql
@@ -1,53 +1,5 @@
 BEGIN;
 
--- todo: fix this in the GTFS feed instead
-CREATE VIEW arrivals_departures_with_fixed_shape_dist_traveled AS
-SELECT
-	arrs_deps.*,
-	CASE
-		WHEN arrs_deps.shape_dist_traveled IS NOT NULL
-		THEN arrs_deps.shape_dist_traveled
-		ELSE closest_shape_pt.shape_dist_traveled
-	END shape_dist_traveled_fixed,
-	st_distance(shape_pt_loc, stops.stop_loc) as dist_to_closes_shape_pt,
-	closest_shape_pt.shape_pt_loc as closest_shape_pt_loc,
-	closest_shape_pt.shape_pt_sequence as closest_shape_pt_seq,
-	closest_shape_pt.shape_dist_traveled as closest_shape_dist_traveled
-FROM arrivals_departures arrs_deps
-INNER JOIN trips ON trips.trip_id = arrs_deps.trip_id
-INNER JOIN stops ON stops.stop_id = arrs_deps.stop_id
-LEFT JOIN LATERAL (
-	SELECT
-		shape_pt_sequence,
-		shape_pt_loc,
-		shape_dist_traveled
-	FROM shapes
-	WHERE shapes.shape_id = arrs_deps.shape_id
-	ORDER BY st_distance(shape_pt_loc, stops.stop_loc)
-	LIMIT 1
-) closest_shape_pt ON True;
-
--- CREATE INDEX ON shapes USING GIST (shape_pt_loc);
--- todo: is a materialized view rellay necessary for better performance?
-DROP VIEW shapes_aggregated;
-CREATE MATERIALIZED VIEW shapes_aggregated AS
-SELECT
-	shape_id,
-	array_agg(shape_dist_traveled) AS distances_travelled,
-	-- todo: is there an ST_MakeLine for geography[]?
-	ST_MakeLine(array_agg(shape_pt_loc::geometry))::geography AS shape
-FROM (
-	SELECT
-		shape_id,
-		shape_dist_traveled,
-		shape_pt_loc
-	FROM shapes
-	ORDER by shape_id, shape_pt_sequence
-) shapes
-GROUP BY shape_id;
-CREATE INDEX ON shapes_aggregated (shape_id);
-CREATE INDEX ON shapes_aggregated USING GIST (shape);
-
 CREATE TABLE vehicle_positions (
 	id SERIAL PRIMARY KEY,
 	vehicle_id TEXT NOT NULL,
@@ -59,44 +11,29 @@ CREATE TABLE vehicle_positions (
 CREATE INDEX ON vehicle_positions (vehicle_id, t);
 -- CREATE INDEX ON vehicle_positions USING GIST (location);
 
-CREATE TYPE matched_run AS (
+CREATE TYPE currently_active_run AS (
 	trip_id TEXT,
 	date TIMESTAMP,
-	shape_id TEXT,
-	nr_of_consec_vehicle_pos BIGINT,
-	t_latest_vehicle_pos TIMESTAMPTZ,
-	latest_vehicle_pos geography(Point, 4326)
+	shape_id TEXT
 );
 
-CREATE FUNCTION all_vehicle_matches(
+CREATE FUNCTION current_runs(
 	_yesterday date,
 	_today date,
 	_t_arrival_min timestamptz,
-	_t_arrival_max timestamptz,
-	_t_vehicle_pos_min timestamptz,
-	_t_vehicle_pos_max timestamptz,
-	_vehicle_id text
+	_t_arrival_max timestamptz
 )
-RETURNS SETOF matched_run
+RETURNS SETOF currently_active_run
 AS $$
-	-- todo: this is ugly, clean it up
-	SELECT *
-	FROM (
 	SELECT
-		DISTINCT ON (shapes.shape_id, vehicle_id)
-		shape_ids.trip_id,
-		shape_ids.date,
-		shape_ids.shape_id,
-		count(pos.pos_id) OVER (PARTITION BY shapes.shape_id ORDER BY trip_id) as nr_of_consec_vehicle_pos,
-		pos.t as t_latest_vehicle_pos,
-		pos.location as latest_vehicle_pos
+		DISTINCT ON (trip_id, date)
+		*
 	FROM (
-		SELECT DISTINCT ON (trips.shape_id)
-			trips.shape_id,
-			trips.trip_id,
-			date
+		SELECT
+			trip_id,
+			date,
+			shape_id
 		FROM arrivals_departures
-		INNER JOIN trips ON trips.trip_id = arrivals_departures.trip_id
 		WHERE True
 		-- cut off by date for better performance
 		AND (date = _yesterday OR date = _today)
@@ -104,65 +41,8 @@ AS $$
 		-- matter if we're excluding some of their arrivals/departures. We just want
 		-- to find *any* arrival/departure of the "run".
 		AND t_arrival >= _t_arrival_min AND t_arrival <= _t_arrival_max
-	) shape_ids
-	INNER JOIN shapes_aggregated shapes ON shapes.shape_id = shape_ids.shape_id
-	INNER JOIN (
-		SELECT
-			id as pos_id,
-			vehicle_id,
-			location,
-			hdop,
-			t
-		FROM vehicle_positions
-		WHERE True
-		AND vehicle_id = _vehicle_id
-		-- When filtering vehicle positions, we pick a longer time range. If the
-		-- vehicle has a delay of n minutes, we still need to find its positions
-		-- *older* than n minutes, in order to reliably identify its "run".
-		AND t >= _t_vehicle_pos_min AND t <= _t_vehicle_pos_max
-		ORDER BY t DESC
-	) pos ON st_dwithin(shapes.shape, pos.location, pos.hdop)
-	) t
-	ORDER BY nr_of_consec_vehicle_pos DESC
-	LIMIT 2;
+		ORDER BY t_arrival ASC
+	) arrs_deps;
 $$ LANGUAGE sql;
-
-CREATE FUNCTION vehicle_match(
-	_yesterday date,
-	_today date,
-	_t_arrival_min timestamptz,
-	_t_arrival_max timestamptz,
-	_t_vehicle_pos_min timestamptz,
-	_t_vehicle_pos_max timestamptz,
-	_vehicle_id text
-)
-RETURNS matched_run
-AS $$
-	DECLARE
-		_sm matched_run;
-		_first matched_run;
-	BEGIN
-		FOR _sm IN
-			SELECT * FROM all_vehicle_matches(
-				_yesterday,
-				_today,
-				_t_arrival_min,
-				_t_arrival_max,
-				_t_vehicle_pos_min,
-				_t_vehicle_pos_max,
-				_vehicle_id
-			)
-		LOOP
-			-- all_vehicle_matches returns <= 2 results
-			IF _first IS NULL THEN
-				_first := _sm;
-			ELSEIF _sm.nr_of_consec_vehicle_pos = _first.nr_of_consec_vehicle_pos THEN
-				RAISE NOTICE '>1 shape with *equal* nr of matching consecutive vehicle positions';
-				RETURN NULL; -- abort
-			END IF;
-		END LOOP;
-		RETURN _first;
-	END;
-$$ LANGUAGE plpgsql;
 
 COMMIT;
